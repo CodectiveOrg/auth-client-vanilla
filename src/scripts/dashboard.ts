2b820fd3404c557c0e553cbc08a044fbe7832822
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-26 11:25:44 +0330

    [ADD] sign out

--- src/scripts/dashboard.ts
+++ src/scripts/dashboard.ts
@@ -22,9 +22,40 @@ function renderSettings(settings: SettingsModel): void {
   settingsSection.innerHTML = `<pre>${content}</pre>`;
 }
 
+async function signOut(): Promise<void> {
+  const response = await fetch("http://localhost:5001/auth/sign-out", {
+    method: "DELETE",
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  window.location.replace("/");
+}
+
+function addListeners(): void {
+  const signOutButton = document.querySelector("#sign-out")!;
+  const errorElement = document.querySelector("#error")!;
+
+  signOutButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    try {
+      await signOut();
+      errorElement.innerHTML = "";
+    } catch (err) {
+      errorElement.innerHTML = (err as Error).message;
+    }
+  });
+}
+
 async function main(): Promise<void> {
   const settings = await fetchSettings();
   renderSettings(settings);
+
+  addListeners();
 }
 
 await main();
